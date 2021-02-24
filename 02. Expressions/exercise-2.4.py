c5ce20f572a08248ec679b294c61d353abd8e6a8
AksPuri <69200881+AksPuri@users.noreply.github.com>
2021-02-24 18:44:11 +0530

    Update exercise-2.4.py
    17 / 2 = 8.5 # float

--- 02. Expressions/exercise-2.4.py
+++ 02. Expressions/exercise-2.4.py
@@ -19,7 +19,7 @@
 width = 17
 height = 12.0
 
-print type(width / 2)    # <type 'int'>
+print type(width / 2)    # <type 'float'>
 print type(width / 2.0)  # <type 'float'>
 print type(height / 3)   # <type 'float'>
 print type(1 + 2 * 5)    # <type 'int'>
